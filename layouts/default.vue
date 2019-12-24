752d36540d2ee31b759b8844f67e88eacb95072f
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2019-12-25 00:17:27 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <loading />
+    </div>
+    <div v-if="currentUser">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+import Loading from "@/components/Loading";
+import { mapState } from "vuex";
+export default {
+  components: { Loading },
+  data() {
+    return {
+      user: {},
+      baseUrl: process.env.BASE_URL
+      // isLoading: true
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.user;
+    },
+    loading() {
+      return this.$store.state.loading;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/chat.vue
@@ -0,0 +1,227 @@
+<template>
+  <div>
+    <header class="header">
+      <h1>Chat</h1>
+      <div v-if="currentUser.uid" key="login">
+        [{{currentUser.displayName}}]
+        <!-- v-onと@一緒 -->
+        <button type="button" @click="signOutGoogle">ログイン</button>
+      </div>
+      <div v-else key="logout">
+        <button type="button" @click="doLogin">"ログイン</button>
+      </div>
+    </header>
+
+    <transition-group name="chat" tag="div" class="list content">
+      <!-- : = v-bind はhtmlの属性に対して使える-->
+      <section
+        v-for="{ key, name, image, message} in chat"
+        :key="key"
+        :class="[name === currentUser.displayName ? 'message-own' : 'message' ]"
+      >
+        <div class="item-image">
+          <img :src="image" width="40" height="40" />
+        </div>
+        <div class="item-detail">
+          <div class="item-name">{{ name }}</div>
+          <div class="item-message">
+            <nl2br tag="div" :text="message" />
+          </div>
+        </div>
+      </section>
+    </transition-group>
+    <form action @submit.prevent="doSend" class="form">
+      <!-- 入力系はv-model -->
+      <!-- enter.exactはenter押した時だけ -->
+      <textarea v-model="input" :disabled="!currentUser.uid" @keydown.enter.exact.prevent="doSend"></textarea>
+      <button type="submit" :disabled="!currentUser.uid" class="send-button">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+import Nl2br from "vue-nl2br";
+// import { mapState } from "vuex";
+
+export default {
+  components: { Nl2br },
+  data() {
+    return {
+      // user: {}, //ユーザー情報
+      chat: [], //取得したメッセージを入れる配列
+      input: "" //入力したメッセージ
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.user;
+    }
+  },
+  mounted() {
+    // console.log("this.currentUser", this.currentUser);
+    const ref_message = firebase.database().ref("message");
+    if (this.currentUser) {
+      this.chat = [];
+      // messageに変更があったときのハンドラを登録
+      ref_message.limitToLast(10).on("child_added", this.childAdded);
+    } else {
+      // messageに変更があったときのハンドラを解除
+      ref_message.limitToLast(10).off("child_added", this.childAdded);
+    }
+    // console.log("this.childAdded", this.childAdded);
+  },
+  methods: {
+    //基本的な関数を入れて置く場所
+    // googleログイン認証のため、userにセットされる値が違うためエラーが出るかも
+    async doLogin() {
+      await this.$store.dispatch("logInGoogle");
+    },
+    signOutGoogle() {
+      this.$store.dispatch("signOutGoogle");
+      this.$router.push("/logIn");
+    },
+    // ログアウト処理
+    doLogout() {
+      firebase.auth().signOut();
+    },
+    // スクロール位置を一番下に移動
+    scrollBottom() {
+      this.$nextTick(() => {
+        window.scrollTo(0, document.body.clientHeight);
+      });
+    },
+    // 受け取ったメッセージをchatに追加
+    // データベースに新しい要素が追加されると随時呼び出される
+    childAdded(snap) {
+      //   let vm = this;
+      const message = snap.val();
+      //chatは最初空配列
+      this.chat.push({
+        key: snap.key,
+        name: message.name,
+        image: message.image,
+        message: message.message
+      });
+      console.log(
+        "message.name === currentUser.displayName",
+        message.name === this.currentUser.displayName
+          ? "message-own"
+          : "message"
+      );
+      this.scrollBottom(); //さっき定義した関数を処理後に呼び出す
+    },
+    doSend() {
+      if (this.currentUser.uid && this.input.length) {
+        let that = this;
+        // firebaseにメッセージを追加
+        firebase
+          .database()
+          .ref("message")
+          .push(
+            {
+              message: this.input,
+              name: this.currentUser.displayName,
+              image: this.currentUser.photoURL
+            },
+            () => {
+              that.input = ""; //フォームを空にする
+            }
+          );
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+.header {
+  background: #3ab383;
+  margin-bottom: 1em;
+  padding: 0.4em 0.8em;
+  color: #fff;
+}
+.content {
+  margin: 0 auto;
+  padding: 0 10px;
+  max-width: 600px;
+}
+.form {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  height: 80px;
+  width: 100%;
+  background: #f5f5f5;
+}
+.form textarea {
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  height: 4em;
+  width: calc(100% - 6em);
+  resize: none;
+}
+.list {
+  margin-bottom: 100px;
+}
+.item {
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 0.8em;
+}
+.item-image img {
+  border-radius: 20px;
+  vertical-align: top;
+}
+.item-detail {
+  margin: 0 0 0 1.4em;
+}
+.item-name {
+  font-size: 75%;
+}
+.item-message {
+  position: relative;
+  display: inline-block;
+  padding: 0.8em;
+  background: #deefe8;
+  border-radius: 4px;
+  line-height: 1.2em;
+}
+.item-message::before {
+  position: absolute;
+  content: " ";
+  display: block;
+  left: -16px;
+  bottom: 12px;
+  border: 4px solid transparent;
+  border-right: 12px solid #deefe8;
+}
+.send-button {
+  height: 4em;
+}
+/* トランジション用スタイル */
+.chat-enter-active {
+  transition: all 1s;
+}
+.chat-enter {
+  opacity: 0;
+  transform: translateX(-1em);
+}
+.message {
+  margin-bottom: 3px;
+}
+.message-own {
+  text-align: right;
+  margin-bottom: 3px;
+}
+.message-own .content {
+  background-color: lightskyblue;
+}
+</style>
